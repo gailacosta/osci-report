68da9230d6f336a3a6f5d6743b13263ccf4494ca
Eric Gardner <gardner.ec@gmail.com>
2016-04-27 15:59:01 -0700

    Remove the "bad old way"

--- extensions/book/opf.rb
+++ extensions/book/opf.rb
@@ -68,49 +68,5 @@ module Book
         f.puts @template
       end
     end
-
-    # def build_opf(metadata)
-      # output_path = "dist/epub/OEBPS/"
-      # filename    = output_path + "content.opf"
-      # template    = get_template("content.opf")
-      # manifest    = ""
-      # spine       = ""
-
-      # Dir.chdir(output_path) do
-        # chapter_files = Dir.glob("*.html")
-        # chapter_files.each_with_index do |chapter, index|
-          # item_id = "chapter#{index + 1}"
-          # manifest << generate_item_tag(chapter, item_id)
-          # spine    << generate_itemref_tag(item_id)
-        # end
-        # assets = Dir.glob("assets/*")
-        # assets.each_with_index do |asset, index|
-          # asset_id = "a#{index + 1}"
-          # if Dir.exist? asset
-            # contents = Dir.glob("#{asset}/*")
-            # contents.each_with_index do |item, index|
-              # asset_id = "b#{index + 1}"
-              # manifest << generate_item_tag(item, asset_id)
-            # end
-          # else
-            # manifest << generate_item_tag(asset, asset_id)
-          # end
-        # end
-      # end
-      # File.open(filename, "w") do |f|
-        # template.at_css("dc|title").content         = metadata[:title]
-        # template.at_css("dc|creator").content       = metadata[:author]
-        # template.at_css("dc|publisher").content     = metadata[:publisher]
-        # template.at_css("dc|date").content          = metadata[:date]
-        # template.at_css("dc|identifier").content    = metadata[:book_id]
-        # modified = Time.now
-        # template.at_css("meta[property='dcterms:modified']").content = modified.utc.iso8601
-        # manifest_contents = Nokogiri::XML::DocumentFragment.parse(manifest)
-        # manifest_contents.parent = template.at_css("manifest")
-        # spine_contents = Nokogiri::XML::DocumentFragment.parse(spine)
-        # spine_contents.parent = template.at_css("spine")
-        # f.puts template
-      # end
-    # end
   end
 end
